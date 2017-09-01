c6409d07f627ba794fe9ef84e6491431805420af
alonamid <alonamid@eecs.berkeley.edu>
2017-09-01 15:02:03 -0700

    fixes for compilation to run

--- src/riscv/ffitarget.h
+++ src/riscv/ffitarget.h
@@ -32,9 +32,9 @@
 #error "Please do not include ffitarget.h directly into your source.  Use ffi.h instead."
 #endif
 
-#ifndef __riscv64
-# error We currently only support RV64.
-#endif
+//#ifndef __riscv64
+//# error We currently only support RV64.
+//#endif
 
 #ifdef __LP64__
 # define FFI_SIZEOF_ARG 8
